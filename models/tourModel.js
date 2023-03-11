be23eb24fbba8838475031275e55228bccd3ec3f
akssingh0102 <akashsingh095@gmail.com>
2023-03-11 21:54:38 +0530

    add DOCUMENT MIDDLEWARE in Tours schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const toursSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const toursSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -56,9 +58,21 @@ const toursSchema = new mongoose.Schema(
   }
 );
 
+// VIRTUAL PROPERTY
 toursSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE : runs before .save() and .create()
+toursSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// toursSchema.post("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", toursSchema);
 

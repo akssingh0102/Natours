da2f23d1522aab4352c83858f4ae98fc051d736a
akssingh0102 <akashsingh095@gmail.com>
2023-03-11 23:17:43 +0530

    Add custom validation to our Tour models

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const toursSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const toursSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, "A tour name must have less or equal then 40 characters"],
       minlength: [10, "A tour name must have more or equal then 10 characters"],
+      // validate: [validator.isAlpha, "Tour name must only contain characters"],
     },
     slug: String,
     duration: {
@@ -24,8 +26,9 @@ const toursSchema = new mongoose.Schema(
       type: String,
       required: [true, "A tour must have a difficulty"],
       enum: {
-        values: ["easy", "medium", "difficult"]},
-        message: "Difficulty must be either easy, medium or difficult"
+        values: ["easy", "medium", "difficult"],
+      },
+      message: "Difficulty must be either easy, medium or difficult",
     },
     ratingsAverage: {
       type: Number,
@@ -41,7 +44,16 @@ const toursSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only point to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) should be bellow regular price",
+      },
+    },
     summary: {
       type: String,
       trim: true,

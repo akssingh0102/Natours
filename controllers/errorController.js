2f66b15a3dc8e42a076389b638c4d118d2739a08
akssingh0102 <akashsingh095@gmail.com>
2023-03-12 16:57:27 +0530

    Handle Error during Development Vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+    // isOperational is out customer field that we store in AppError class (trusted errors)
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error (don't leak details to client)
+  } else {
+    console.error("ERROR 💥: ", err);
+    res.status(500).json({
+      status: "error",
+      message: "something went very wrong !",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };

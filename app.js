4c18daf3a08c9d36aba87ec5ad896f76d4530549
akssingh0102 <akashsingh095@gmail.com>
2023-03-12 10:40:57 +0530

    Handle Unhandled Routes

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRouters');
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRouters");
 
 // 1. MIDDLEWARE
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // 2. ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server !`,
+  });
+  next();
+});
 
 module.exports = app;

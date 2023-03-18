2d3c510536dfbf13449511698cc74f1423661dcd
akssingh0102 <akashsingh095@gmail.com>
2023-03-18 22:27:55 +0530

    signing up user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,21 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+var jwt = require("jsonwebtoken");
 
+// @desc Signup a user
+// @route POST /api/v1/users/signup
+// @access public
 const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({ name, email, password, passwordConfirm });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       tour: newUser,
     },

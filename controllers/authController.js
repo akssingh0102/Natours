31d5deeb582208e8e49dc9e7f637318282a52334
akssingh0102 <akashsingh095@gmail.com>
2023-03-19 08:07:16 +0530

    Login users API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 var jwt = require("jsonwebtoken");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 // @desc Signup a user
 // @route POST /api/v1/users/signup
@@ -9,9 +16,7 @@ const signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
   const newUser = await User.create({ name, email, password, passwordConfirm });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -22,6 +27,31 @@ const signup = catchAsync(async (req, res, next) => {
   });
 });
 
+// @desc Login a user
+// @route POST /api/v1/users/login
+// @access public
+const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.isCorrectPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password"), 401);
+  }
+
+  const token = signToken(user._id);
+  
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
+
 module.exports = {
   signup,
+  login,
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,37 @@
-const getAllUsers = (req, res) => {
-  res.status(500).send({
-    status: 'error',
-    message: 'This route is not yet defined !',
-  });
-};
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+const getAllUsers = catchAsync(async (req, res) => {
+  const user = await User.find();
+
+  res.json({ status: "success", results: user.length, data: user });
+});
 
 const createUser = (req, res) => {
   res.status(500).send({
-    status: 'error',
-    message: 'This route is not yet defined !',
+    status: "error",
+    message: "This route is not yet defined !",
   });
 };
 
 const getUser = (req, res) => {
   res.status(500).send({
-    status: 'error',
-    message: 'This route is not yet defined !',
+    status: "error",
+    message: "This route is not yet defined !",
   });
 };
 
 const updateUser = (req, res) => {
   res.status(500).send({
-    status: 'error',
-    message: 'This route is not yet defined !',
+    status: "error",
+    message: "This route is not yet defined !",
   });
 };
 
 const deleteUser = (req, res) => {
   res.status(500).send({
-    status: 'error',
-    message: 'This route is not yet defined !',
+    status: "error",
+    message: "This route is not yet defined !",
   });
 };
 

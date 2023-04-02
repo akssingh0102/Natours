3eeed5ad6f4bfb301db3d1a18866f8ffe2bbec56
akssingh0102 <akashsingh095@gmail.com>
2023-04-02 21:33:12 +0530

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,14 +173,14 @@ const resetPassword = catchAsync(async (req, res, next) => {
   });
 
   // 2) If token has not expired, and there is user, set the new password
-  if (!user) { 
+  if (!user) {
     return next(new AppError("Token is invalid or has expired", 400));
   }
 
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
   user.passwordResetToken = undefined;
-  user.passwordResetExpires = undefined; 
+  user.passwordResetExpires = undefined;
   await user.save();
 
   // 3) Update changedPasswordAt property for the user
